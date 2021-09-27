b21b73b717a715f2572ee89a6606d24441ae7777
Alfredo Chico <alfredochico429@gmail.com>
2021-09-27 14:07:45 -0500

    Modificación de los labels de las tablas cat-puesto, cat-sucursal y empleado

--- views/cat-puesto/create.php
+++ views/cat-puesto/create.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\CatPuesto */
 
-$this->title = 'Create Cat Puesto';
+$this->title = 'Crear Puesto';
 $this->params['breadcrumbs'][] = ['label' => 'Cat Puestos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
--- views/empleado/create.php
+++ views/empleado/create.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Empleado */
 
-$this->title = 'Create Empleado';
+$this->title = 'Añadir empleado';
 $this->params['breadcrumbs'][] = ['label' => 'Empleados', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
