fab691bb0cf039cd893f7c6b16211d6a97b9fa57
Magdiel <yonosoymagdiel@gmail.com>
2021-09-29 20:09:44 -0500

    modificacion de crud pagos

--- views/pago/index.php
+++ views/pago/index.php
@@ -15,7 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Pago', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Crear pago', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
--- views/pago/view.php
+++ views/pago/view.php
@@ -16,11 +16,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->pag_id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->pag_id], [
+        <?= Html::a('Actualizar', ['update', 'id' => $model->pag_id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Eliminar', ['delete', 'id' => $model->pag_id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => '¿Estas seguro que deseas eliminar este elemento?',
                 'method' => 'post',
             ],
         ]) ?>
