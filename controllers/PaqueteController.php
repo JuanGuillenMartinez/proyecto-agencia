bf4026b9d1444cd523d6a4f82d770aa91f867074
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2021-10-08 22:57:02 -0500

    pruebas

--- controllers/PaqueteController.php
+++ controllers/PaqueteController.php
@@ -66,6 +66,7 @@ class PaqueteController extends Controller
         if ($this->request->isPost) {
             if ($model->load($this->request->post())) {
                 $image = UploadedFile::getInstance($model, 'img');
+                $var = "hola";
                 return $this->redirect(['view', 'id' => $model->paq_id]);
             }
         } else {
