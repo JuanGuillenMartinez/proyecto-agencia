0463ee7c18587ef35fe8bd032e007c4889f6633f
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2021-10-11 13:18:57 -0500

    Cambios

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,67 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" style="background: url(&quot;https://cdn.bootstrapstudio.io/placeholders/1400x800.png&quot;);">
 
-/* @var $this yii\web\View */
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>botonera</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&amp;display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+</head>
 
-$this->title = 'My Yii Application';
-?>
-<div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+<body style="background: #56baed;">
+    <section class="border rounded" style="display: inline-block;margin-left: 48px;margin-right: 48px;background: #ffffff;margin-top: 48px;margin-bottom: 48px;border-radius: 80px;border-width: 10px;border-style: none;">
+        <div style="margin-top: 24px;">
+            <h1 class="text-uppercase" style="text-align: center;padding-bottom: 8px;font-size: 50px;font-family: Poppins, sans-serif;padding-top: 8px;font-weight: bold;">Agencia de viajes</h1>
         </div>
+        <div class="row" style="margin-top: 36px;font-family: Roboto, sans-serif;font-size: 14px;">
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="rounded-circle img-fluid" src="/web/img/icons/reservacion.png" width="40%">
+                <h2 style="margin-top: 8px;">Reservacion</h2>
+            </div>
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="img-fluid" data-bss-hover-animate="pulse" src="/web/img/icons/vuelo.png" width="40%">
+                <h2 style="margin-top: 8px;">Paquete</h2>
+            </div>
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="rounded img-fluid" data-bss-hover-animate="pulse" width="40%" src="/web/img/icons/seguro.png">
+                <h2 style="margin-top: 8px;">Seguro</h2>
+            </div>
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="rounded img-fluid" data-bss-hover-animate="pulse" width="40%" src="/web/img/icons/region.png">
+                <h2 style="margin-top: 8px;">Región</h2>
+            </div>
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="rounded img-fluid" data-bss-hover-animate="pulse" src="/web/img/icons/alojamiento.png" width="40%">
+                <h2 style="margin-top: 8px;">Alojamiento</h2>
+            </div>
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="rounded img-fluid" data-bss-hover-animate="pulse" src="/web/img/icons/aeropuerto.png" width="40%">
+                <h2 style="margin-top: 8px;">Aeropuerto</h2>
+            </div>
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="rounded img-fluid" data-bss-hover-animate="pulse" width="40%" src="/web/img/icons/pais.png">
+                <h2 style="margin-top: 8px;">País</h2>
+            </div>
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="rounded img-fluid" data-bss-hover-animate="pulse" src="/web/img/icons/ubicación.png" width="40%">
+                <h2 style="margin-top: 8px;">Ubicación</h2>
+            </div>
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="rounded img-fluid" data-bss-hover-animate="pulse" src="/web/img/icons/vuelo.png" width="40%">
+                <h2 style="margin-top: 8px;">Vuelo</h2>
+            </div>
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="rounded img-fluid" data-bss-hover-animate="pulse" width="40%" src="/web/img/icons/puesto.png">
+                <h2 style="margin-top: 8px;">Puesto</h2>
+            </div>
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="rounded img-fluid" data-bss-hover-animate="pulse" src="/web/img/icons/sucursal.png" width="40%">
+                <h2 style="margin-top: 8px;">Sucursal</h2>
+            </div>
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="rounded img-fluid" data-bss-hover-animate="pulse" width="40%" src="/web/img/icons/empleado.png">
+                <h2 style="margin-top: 8px;">Empleado</h2>
+            </div>
+            <div class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"></div>
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="rounded img-fluid" data-bss-hover-animate="pulse" width="40%" src="/web/img/icons/factura.png">
+                <h2 style="margin-top: 8px;">Factura</h2>
+            </div>
+            <div data-bss-hover-animate="pulse" class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"><img class="rounded img-fluid" data-bss-hover-animate="pulse" src="/web/img/icons/persona.png" width="40%">
+                <h2 style="margin-top: 8px;">Persona</h2>
+            </div>
+            <div class="col-md-3" style="text-align: center;padding-top: 8px;padding-bottom: 8px;margin-bottom: 12px;"></div>
+        </div>
+    </section>
+</body>
 
-    </div>
-</div>
+</html>
