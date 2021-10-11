4e66a70fe7a271d1486b97f475b775fd8e1f8e22
Magdiel <yonosoymagdiel@gmail.com>
2021-10-11 18:52:03 -0500

    CAMBIO DEL ACTIVEFORM

--- views/pago/_form.php
+++ views/pago/_form.php
@@ -3,7 +3,7 @@
 use app\models\Pago;
 use yii\helpers\Html;
 use kartik\select2\Select2;
-use yii\widgets\ActiveForm;
+use yii\bootstrap4\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Pago */
