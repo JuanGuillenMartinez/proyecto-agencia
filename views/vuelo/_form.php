d88ee25474005c552a4046b52e751a2ffe6840ff
Luis Emilio Cristobal Serra <ecris1502@gmail.com>
2021-10-13 14:23:09 -0500

    Corrección del select2 en tabla Vuelo

--- views/vuelo/_form.php
+++ views/vuelo/_form.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use kartik\widgets\Select2;
 use yii\widgets\ActiveForm;
 use app\models\CatAerolinea;
+use app\models\CatUbicacion;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Vuelo */
@@ -30,14 +31,32 @@ use app\models\CatAerolinea;
 
     <?php //= $form->field($model, 'vue_fkaerolinea')->textInput() ?>
 
-    <?= $form->field($model, 'vue_fkaeroorigen')->textInput() ?>
+    <?php //= $form->field($model, 'vue_fkaeroorigen')->textInput() ?>
 
-    <?= $form->field($model, 'vue_fkaerodestino')->textInput() ?>
+    <?php //= $form->field($model, 'vue_fkaerodestino')->textInput() ?>
 
     <?=$form->field($model, 'vue_fkaerolinea')->widget(Select2::classname(), [
     'data' => CatAerolinea::map(),
     'language' => 'es',
     'options' => ['placeholder' => 'Selecciona una aerolinea...'],
+    'pluginOptions' => [
+        'allowClear' => true
+    ],
+    ]);?>
+
+<?=$form->field($model, 'vue_fkaeroorigen')->widget(Select2::classname(), [
+    'data' => CatUbicacion::map(),
+    'language' => 'es',
+    'options' => ['placeholder' => 'Selecciona el aeropuerto de origen...'],
+    'pluginOptions' => [
+        'allowClear' => true
+    ],
+    ]);?>
+
+<?=$form->field($model, 'vue_fkaerodestino')->widget(Select2::classname(), [
+    'data' => CatUbicacion::map(),
+    'language' => 'es',
+    'options' => ['placeholder' => 'Selecciona el aeropuerto de destino...'],
     'pluginOptions' => [
         'allowClear' => true
     ],
