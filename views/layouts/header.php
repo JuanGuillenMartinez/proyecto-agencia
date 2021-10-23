342275947cc284c639514fe5d74a78ebdd7875d8
Alfredo Chico <alfredochico429@gmail.com>
2021-10-22 21:54:12 -0500

    Navbar

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,34 +1,38 @@
 <?php
 
+use yii\bootstrap4\Nav;
 use yii\bootstrap4\Html;
+use yii\bootstrap4\NavBar;
+use webvimark\modules\UserManagement\UserManagementModule;
 ?>
-<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle dark"><i></i></a>
-<!-- <div id="fh5co-logo"><img src="/web/img/logo.png" alt=""><a href="index.html"><i class="icon-airplane"></i>Travel</a></div> -->
-<!-- START #fh5co-menu-wrap -->
-<div class="row">
-    <div class="col-md-6">
-        <div id="fh5co-logo"><?= Html::img("/img/logo.png") ?> <a href="index.html">Travel</a> </div>
-    </div>
-    <div class="col-md-6">
-        <nav id="fh5co-menu-wrap" role="navigation">
-            <ul class="sf-menu" id="fh5co-primary-menu">
-                <li class="active"><a href="index.html">Inicio</a></li>
-                <li>
-                    <a href="vacation.html" class="fh5co-sub-ddown">Paquetes</a>
-                    <ul class="fh5co-sub-menu">
-                        <li><a href="#">Family</a></li>
-                        <li><a href="#">CSS3 &amp; HTML5</a></li>
-                        <li><a href="#">Angular JS</a></li>
-                        <li><a href="#">Node JS</a></li>
-                        <li><a href="#">Django &amp; Python</a></li>
-                    </ul>
-                </li>
-                <li><a href="flight.html">Vuelos</a></li>
-                <li><a href="hotel.html">Hoteles</a></li>
-                <li><a href="car.html">Traslados</a></li>
-                <li><a href="blog.html">Seguros</a></li>
-                <!-- <li><a href="contact.html">Iniciar sesion</a></li> -->
-            </ul>
-        </nav>
-    </div>
-</div>
+<?php
+    NavBar::begin([
+        'brandLabel' => Html::img("/img/globe.png",['style' => 'width:50px; height:50px']).' '.Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'collapseOptions' => ['class' => 'justify-content-end'],
+        'options' => [
+            'class' => 'navbar navbar-expand-md fixed-top',
+            'id' => "fh5co-menu-wrap",
+            'style' => 'width:90%;'
+        ],
+    ]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav sf-menu', 'id' => "fh5co-primary-menu"],
+        'encodeLabels' => false,
+        'items' => [
+            ['label' => 'Paquetes', 'url' => ['/site/contact']],
+            ['label' => 'Vuelos', 'url' => ['/site/index']],
+            ['label' => 'Hoteles', 'url' => ['/site/about']],
+            ['label' => 'Traslados', 'url' => ['/site/contact']],
+            ['label' => 'Seguros', 'url' => ['/site/contact']],
+            [
+                'label' => 'Administrador', 
+                'items'=>UserManagementModule::menuItems(), 
+                'visible' => Yii::$app->user->isSuperAdmin
+            ],
+            ['label'=>'Login', 'url'=>['/user-management/auth/login'], 'visible' => Yii::$app->user->isGuest],
+			['label'=>'Logout', 'url'=>['/user-management/auth/logout'], 'visible' => !Yii::$app->user->isGuest],
+        ],
+    ]);
+    NavBar::end();
+?>
