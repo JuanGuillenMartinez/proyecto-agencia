bd11b1b7218195c40da27797f35027f8a6c4d857
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2021-10-25 13:16:05 -0500

    Cambios en paquetes populares

--- views/plantilla/index.php
+++ views/plantilla/index.php
@@ -54,7 +54,7 @@
                 </div>
 
                 <div class="row">
-                    <?= $this->render('/plantilla/paquetes_populares'); ?>
+                    <?= $this->render('/plantilla/paquetes-populares'); ?>
                     <div class="col-md-12 text-center animate-box">
                         <p><a class="btn btn-primary btn-outline btn-lg" href="/paquete">Ver todas las ofertas <i class="icon-arrow-right22"></i></a></p>
                     </div>
@@ -242,67 +242,7 @@
         </div>
 
         <div id="fh5co-blog-section" class="fh5co-section-gray">
-            <div class="container">
-                <div class="row justify-content-md-center">
-                    <div class="col-md-8 text-center heading-section animate-box">
-                        <h3>Paquetes recientes</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est facilis maiores, perspiciatis accusamus asperiores sint consequuntur debitis.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <div class="row row-bottom-padded-md">
-                    <div class="col-lg-4 col-md-4 col-sm-6">
-                        <div class="fh5co-blog animate-box">
-                            <a href="#"><img class="img-responsive" src="/plantilla/images/place-1.jpg" alt=""></a>
-                            <div class="blog-text">
-                                <div class="prod-title">
-                                    <h3><a href="#">30% Discount to Travel All Around the World</a></h3>
-                                    <span class="posted_by">Sep. 15th</span>
-                                    <span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                                    <p><a href="#">Learn More...</a></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-6">
-                        <div class="fh5co-blog animate-box">
-                            <a href="#"><img class="img-responsive" src="/plantilla/images/place-2.jpg" alt=""></a>
-                            <div class="blog-text">
-                                <div class="prod-title">
-                                    <h3><a href="#">Planning for Vacation</a></h3>
-                                    <span class="posted_by">Sep. 15th</span>
-                                    <span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                                    <p><a href="#">Learn More...</a></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="clearfix visible-sm-block"></div>
-                    <div class="col-lg-4 col-md-4 col-sm-6">
-                        <div class="fh5co-blog animate-box">
-                            <a href="#"><img class="img-responsive" src="/plantilla/images/place-3.jpg" alt=""></a>
-                            <div class="blog-text">
-                                <div class="prod-title">
-                                    <h3><a href="#">Visit Tokyo Japan</a></h3>
-                                    <span class="posted_by">Sep. 15th</span>
-                                    <span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                                    <p><a href="#">Learn More...</a></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="clearfix visible-md-block"></div>
-                </div>
-
-                <div class="col-md-12 text-center animate-box">
-                    <p><a class="btn btn-primary btn-outline btn-lg" href="#">See All Post <i class="icon-arrow-right22"></i></a></p>
-                </div>
-
-            </div>
+            <?= $this->render('paquetes-recientes'); ?>
         </div>
         <!-- fh5co-blog-section -->
         <div id="fh5co-testimonial" style="background-image:url(/plantilla/images/img_bg_1.jpg);">
