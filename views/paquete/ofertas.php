313d06e75094080cbc65051c291ee1427cb02ec6
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2021-10-26 18:53:22 -0500

    Cambios en el index

--- views/paquete/ofertas.php
+++ views/paquete/ofertas.php
@@ -13,51 +13,16 @@ foreach ($paquetes as $paquete) {
     echo "
     <div class='desc'>
             <span></span>
+            <div class'ofertas-description'>
             <h3>{$paquete->paq_nombre}</h3>
             <span>{$paquete->paqFkvuelo->getVueloInfo()}</span>
-            <span class='price'>$ {$paquete->paq_subtotal}</span>
+            <span style='text-decoration-line: line-through;' class='price'>$ {$paquete->paq_subtotal}</span>
             <span class='descuento'>Hasta {$paquete->paq_descuento}%</span>
             <a class='btn btn-primary btn-outline' href='#'>Comprar ahora</a>
+            </div>
     </div>
     ";
     echo "</div>";
     echo '</div>';
 }
 ?>
-
-<!-- <div class="col-md-4 col-sm-6 fh5co-tours animate-box" data-animate-effect="fadeIn">
-    <div>
-        <img src="/plantilla/images/place-1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-        <div class="desc">
-            <span></span>
-            <h3>New York</h3>
-            <span>3 nights + Flight 5*Hotel</span>
-            <span class="price">$1,000</span>
-            <a class="btn btn-primary btn-outline" href="#">Book Now <i class="icon-arrow-right22"></i></a>
-        </div>
-    </div>
-</div>
-<div class="col-md-4 col-sm-6 fh5co-tours animate-box" data-animate-effect="fadeIn">
-    <div>
-        <img src="/plantilla/images/place-2.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-        <div class="desc">
-            <span></span>
-            <h3>Philippines</h3>
-            <span>4 nights + Flight 5*Hotel</span>
-            <span class="price">$1,000</span>
-            <a class="btn btn-primary btn-outline" href="#">Book Now <i class="icon-arrow-right22"></i></a>
-        </div>
-    </div>
-</div>
-<div class="col-md-4 col-sm-6 fh5co-tours animate-box" data-animate-effect="fadeIn">
-    <div>
-        <img src="/plantilla/images/place-3.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-        <div class="desc">
-            <span></span>
-            <h3>Hongkong</h3>
-            <span>2 nights + Flight 4*Hotel</span>
-            <span class="price">$1,000</span>
-            <a class="btn btn-primary btn-outline" href="#">Book Now <i class="icon-arrow-right22"></i></a>
-        </div>
-    </div>
-</div> -->
