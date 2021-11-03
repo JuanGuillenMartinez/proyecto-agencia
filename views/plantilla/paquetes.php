ddab0894b2df356d7f212692ea3c20f668b91e18
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2021-11-03 12:12:09 -0600

    Cambios en el index del sitio

--- views/plantilla/paquetes.php
+++ views/plantilla/paquetes.php
@@ -8,26 +8,32 @@ use yii\helpers\Html;
 
 <div class="container">
     <div class="main-card-paquetes">
+        <div class="text row justify-content-md-center">
+            <div class="col-md-8 text-center heading-section animate-box">
+                <h3>Conoce todos nuestros paquetes</h3>
+                <p>Nosotros le ofrecemos el siguiente catalogo de paquetes disponibles para usted.</p>
+            </div>
+        </div>
         <ul class="cards">
-            <?php 
-                $paquetes = Paquete::find()->all();
-                foreach($paquetes as $paquete) {
-                    echo '<li class="cards_item">
+            <?php
+            $paquetes = Paquete::find()->all();
+            foreach ($paquetes as $paquete) {
+                echo '<li class="cards_item">
                     <div class="card-paquete">
                     <div class="card_image">';
-                    $urlImagen = '@web/img/paquete/' . $paquete->paq_url;
-                    echo Html::img($urlImagen, ['class' => 'image-card']);
-                    echo '</div>';
-                    echo "
+                $urlImagen = '@web/img/paquete/' . $paquete->paq_url;
+                echo Html::img($urlImagen, ['class' => 'image-card']);
+                echo '</div>';
+                echo "
                     <div class='card_content'>
                     <h2 class='card_title'>{$paquete->getDestinoVuelo()}</h2>
                     <p class='card_text'>{$paquete->paqFkvuelo->getVueloInfo()}</p>
                     <button class='btn-card-paquetes card_btn'>Leer más</button>
                 </div>
                     ";
-                    echo '</div>
+                echo '</div>
                     </li>';
-                }
+            }
             ?>
         </ul>
     </div>
