9c78c139e42dcace25b7adbf08798866de058f84
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2021-12-01 21:07:54 -0600

    Cambios

--- views/plantilla/paquetes.php
+++ views/plantilla/paquetes.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use app\models\Paquete;
 
 ?>
 <?= $this->render("/paquete/mejor_oferta", compact("paquete")); ?>
---
+++ widgets/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=fdb34.awardspace.net;dbname=3948827_agencia',
+    'username' => '3948827_agencia',
+    'password' => 'b]ES,HMl7BjLH?Y:',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
